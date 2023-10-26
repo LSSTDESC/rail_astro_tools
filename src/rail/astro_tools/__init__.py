0028406ddf2b8a35289913a80596a6e3f14135e5
Eric Charles <badass@stanford.edu>
2023-10-25 18:34:47 -0700

    Moved utilPhotometry to util_photometry

--- src/rail/astro_tools/__init__.py
+++ src/rail/astro_tools/__init__.py
@@ -5,4 +5,4 @@ from rail.creation.degradation.observing_condition_degrader import *
 from rail.creation.degradation.spectroscopic_degraders import *
 from rail.creation.degradation.spectroscopic_selections import *
 from rail.creation.degradation.lsst_error_model import *
-from rail.tools.utilPhotometry import *
+from rail.tools.util_photometry import *

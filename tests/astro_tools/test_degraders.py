e335cc470dba5ef17e1c4e9c2ea512416ea37e1e
Tianqing Zhang <ztq1996@live.com>
2024-05-07 19:38:43 -0400

    change find_rail_file import path

--- tests/astro_tools/test_degraders.py
+++ tests/astro_tools/test_degraders.py
@@ -7,7 +7,7 @@ import pytest
 from photerr import LsstErrorModel as PhoterrErrorModel
 
 from rail.core.data import DATA_STORE, TableHandle
-from rail.utils.core_utils import find_rail_file
+from rail.utils.path_utils import find_rail_file
 from rail.utils.stage_utils import ColumnMapper
 from rail.creation.degradation.spectroscopic_degraders import InvRedshiftIncompleteness, LineConfusion
 from rail.creation.degradation.spectroscopic_selections import *

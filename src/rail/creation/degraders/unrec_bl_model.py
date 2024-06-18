96ecbbf8bd509982b5fa18f07af5aeebf39b463d
shuang92 <sliang92@stanford.edu>
2024-06-18 15:12:38 -0700

    add FoF

--- src/rail/creation/degraders/unrec_bl_model.py
+++ src/rail/creation/degraders/unrec_bl_model.py
@@ -1,22 +1,23 @@
 """Model for Creating Unrecognized Blends"""
 
-from dataclasses import MISSING
 from ceci.config import StageParameter as Param
 from rail.creation.degrader import Degrader
-from astropy.coordinates import SkyCoord
 import numpy as np, pandas as pd
+import FoFCatalogMatching
 
 class UnrecBlModel(Degrader):
     """Model for Creating Unrecognized Blends.
 
     Finding objects nearby each other. Merge them into one blended
+    Use Friends of Friends for matching. May implement shape matching in the future.
+    Take avergaged Ra and Dec for blended source, and sum up fluxes in each band. May implement merged shapes in the future.
 
     """
     name = "UnrecBlModel"
     config_options = Degrader.config_options.copy()
-    config_options.update(ra=Param(str, 'ra', msg='ra column name'),
-                          dec=Param(str, 'dec', msg='dec column name'),
-                          dist_cut=Param(float, 2.0, msg='distance cut for blends'),
+    config_options.update(ra_label=Param(str, 'ra', msg='ra column name'),
+                          dec_label=Param(str, 'dec', msg='dec column name'),
+                          linking_lengths=Param(float, 1.0, msg='linking_lengths for FoF matching'),
                           bands=Param(str, 'ugrizy', msg='name of filters'),
                           match_size=Param(bool, False, msg='consider object size for finding blends'),
                           match_shape=Param(bool, False, msg='consider object shape for finding blends'),
@@ -29,41 +30,40 @@ class UnrecBlModel(Degrader):
         """
         Constructor
 
-        Does standard Degrader initialization and sets up the error model.
+        Does standard Degrader initialization and sets up the bl model.
         """
         Degrader.__init__(self, args, comm=comm)
 
     def __match_bl__(self, data):
 
-        """Find nearest neighbor of each object within certain radius."""
+        """Group sources with friends of friends"""
 
-        N_data = len(data)
-        group_id = np.linspace(0, N_data-1, N_data, dtype='int')
+        ra_label, dec_label = self.config.ra_label, self.config.dec_label
+        linking_lengths = self.config.linking_lengths
 
-        ra_name, dec_name = self.config.ra, self.config.dec
-        coords = SkyCoord(ra=data[ra_name], dec=data[dec_name], unit='deg')
-        idx, d2d, d3d = coords.match_to_catalog_sky(coords, nthneighbor=2)
+        results = FoFCatalogMatching.match({'truth': data}, linking_lengths=linking_lengths, ra_label=ra_label, dec_label=dec_label)
+        results.remove_column('catalog_key')
 
-        for i in range(N_data):
-            if d2d.arcsec[i] < self.config.dist_cut:
-                group_id[idx[i]] = group_id[i]
+        results = results.to_pandas(index='row_index')
+        results.sort_values(by='row_index', inplace=True)
 
-        data['bl_group_id'] = group_id
+        ## adding the group id as the last column to data
+        matchData = pd.merge(data, results, left_index=True, right_index=True)
 
-        return data
+        return matchData
 
     def __merge_bl__(self, data):
 
-        """Merge nearest neighbors into unrecognized blends."""
+        """Merge sources within a group into unrecognized blends."""
         
-        group_id = data['bl_group_id']
+        group_id = data['group_id']
         unique_id = np.unique(group_id)
 
-        ra_name, dec_name = self.config.ra, self.config.dec
+        ra_label, dec_label = self.config.ra_label, self.config.dec_label
 
-        cols = list(data.columns[:-1])   ## exluding the group id
-        ra_ind = cols.index(ra_name)
-        dec_ind = cols.index(dec_name)
+        cols = list(data.columns)
+        ra_ind = cols.index(ra_label)
+        dec_ind = cols.index(dec_label)
         bands_ind = {b:cols.index(b) for b in self.config.bands}
 
         N_rows = len(unique_id)
@@ -73,16 +73,19 @@ class UnrecBlModel(Degrader):
         
         for i, id in enumerate(unique_id):
 
-            this_group = data.query(f'bl_group_id=={id}')
+            this_group = data.query(f'group_id=={id}')
 
-            mergeData[i, ra_ind] = this_group[ra_name].mean()
-            mergeData[i, dec_ind] = this_group[dec_name].mean()
+            ## take the average position for the blended source
+            mergeData[i, ra_ind] = this_group[ra_label].mean()
+            mergeData[i, dec_ind] = this_group[dec_label].mean()
 
+            ## sum up the fluxes into the blended source
             for b in self.config.bands:
                   mergeData[i, bands_ind[b]] = -2.5*np.log10(np.sum(10**(-this_group[b]/2.5)))
 
+        mergeData[:,-1] = unique_id
         mergeData_df = pd.DataFrame(data=mergeData, columns=cols)
-        mergeData_df["bl_group_id"] = unique_id
+        mergeData_df['group_id'] = mergeData_df['group_id'].astype(int)
 
         return mergeData_df
 

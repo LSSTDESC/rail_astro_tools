13d5f531cb1a29cc72cd0926e8a80d27d79987a5
Eric Charles <badass@stanford.edu>
2024-08-02 15:25:56 -0700

    Update __init__.py

--- src/rail/astro_tools/__init__.py
+++ src/rail/astro_tools/__init__.py
@@ -4,6 +4,6 @@ from rail.creation.degraders.grid_selection import *
 from rail.creation.degraders.observing_condition_degrader import *
 from rail.creation.degraders.spectroscopic_degraders import *
 from rail.creation.degraders.spectroscopic_selections import *
-from rail.creation.degraders.lsst_error_model import *
+from rail.creation.degraders.photometric_errors import *
 from rail.creation.degraders.unrec_bl_model import *
 from rail.tools.photometry_tools import *

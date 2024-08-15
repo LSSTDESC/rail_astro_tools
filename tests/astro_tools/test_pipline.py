ce379edbc410f1d10a71c759d3cc6256fd3f8cb9
Tianqing Zhang <ztq1996@live.com>
2024-08-15 16:14:27 -0400

    rename

--- tests/astro_tools/test_pipline.py
+++ tests/astro_tools/test_pipline.py
@@ -6,9 +6,9 @@ import pytest
 @pytest.mark.parametrize(
     "pipeline_class",
     [
-        'rail.pipelines.utility.apply_phot_errors.ApplyPhotErrorsPipeline',
-        'rail.pipelines.utility.blending.BlendingPipeline',
-        'rail.pipelines.utility.spectroscopic_selection_pipeline.SpectroscopicSelectionPipeline',
+        'rail.pipelines.degradation.apply_phot_errors.ApplyPhotErrorsPipeline',
+        'rail.pipelines.degradation.blending.BlendingPipeline',
+        'rail.pipelines.degradation.spectroscopic_selection_pipeline.SpectroscopicSelectionPipeline',
     ]
 )
 def test_build_and_read_pipeline(pipeline_class):

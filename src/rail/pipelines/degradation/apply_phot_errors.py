6d663754c615f7a8568203d10b5fccac95019ec9
Eric Charles <badass@stanford.edu>
2025-12-01 15:59:27 -0800

    Issue/19/degrade pipes (#80)
    * Allow parallel mode for truth_to_observed and apply_phot_errors pipeline, and switch to using catalog_tag
    
    * switch bands from str to list
    
    * apply tag when testing pipeline
    
    * use catalog names for spectroscopic_selection_pipeline.py
    
    * updated rail_base requirement

--- src/rail/pipelines/degradation/apply_phot_errors.py
+++ src/rail/pipelines/degradation/apply_phot_errors.py
@@ -26,6 +26,12 @@ ERROR_MODELS = dict(
         ErrorModel='LSSTErrorModel',
         Module='rail.creation.degraders.photometric_errors',
         Bands=['u', 'g', 'r', 'i', 'z', 'y'],
+        Overrides=dict(
+            minorCol='minor',
+            majorCol='major',
+            extendedType='gaap',
+            hdf5_groupname='',
+        )
     ),
     #roman = dict(
     #    ErrorModel='RomanErrorModel',
@@ -44,7 +50,12 @@ class ApplyPhotErrorsPipeline(RailPipeline):
 
     default_input_dict = dict(input='dummy.in')
 
-    def __init__(self, error_models=None):
+    def __init__(
+        self,
+        error_models: dict|None=None,
+        *,
+        parallel: bool=False,
+    ):
         RailPipeline.__init__(self)
 
         DS = RailStage.data_store
@@ -56,29 +67,38 @@ class ApplyPhotErrorsPipeline(RailPipeline):
         self.reddener = Reddener.build(
             dustmap_dir=dustmap_dir,
             copy_all_cols=True,
-        )        
+        )
         previous_stage = self.reddener
-        full_rename_dict = catalog_utils.CatalogConfigBase.active_class().band_name_dict()
+        full_rename_dict = catalog_utils.get_active_tag().band_name_dict()
         for key, val in error_models.items():
             error_model_class = ceci.PipelineStage.get_stage(val['ErrorModel'], val['Module'])
             if 'Bands' in val:
                 rename_dict = {band_: full_rename_dict[band_] for band_ in val['Bands']}
             else:  # pragma: no cover
                 rename_dict = full_rename_dict
+            overrides = val.get('Overrides', {})
             the_error_model = error_model_class.make_and_connect(
                 name=f'error_model_{key}',
                 connections=dict(input=previous_stage.io.output),
-                hdf5_groupname='',
                 renameDict=rename_dict,
-                minorCol='minor',
-                majorCol='major',
-                extendedType='gaap',
+                **overrides,
             )
             self.add_stage(the_error_model)
-            previous_stage = the_error_model
+            if parallel:
+                the_dereddener = Dereddener.make_and_connect(
+                    name=f'deredden_{key}',
+                    dustmap_dir=dustmap_dir,
+                    connections=dict(input=the_error_model.io.output),
+                    copy_all_cols=True,
+                )
+                self.add_stage(the_dereddener)
+                previous_stage = self.reddener
+            else:
+                previous_stage = the_error_model
 
-        self.dereddener_errors = Dereddener.build(
-            dustmap_dir=dustmap_dir,
-            connections=dict(input=previous_stage.io.output),
-            copy_all_cols=True,
-        )
+        if not parallel:
+            self.dereddener_errors = Dereddener.build(
+                dustmap_dir=dustmap_dir,
+                connections=dict(input=previous_stage.io.output),
+                copy_all_cols=True,
+            )

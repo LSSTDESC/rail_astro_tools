bf60c3dda26380ef3d4d7faf762183af118d5acb
sschmidt23 <sschmidt@physics.ucdavis.edu>
2025-12-01 12:59:03 -0800

    add unit test call

--- tests/astro_tools/test_degraders.py
+++ tests/astro_tools/test_degraders.py
@@ -217,6 +217,12 @@ def test_SpecSelection(data):
 
     os.remove(degrader_DEEP2_LSST.get_output(degrader_DEEP2_LSST.get_aliased_tag("output"), final_name=True))
 
+    degrader_DEEP2_LSST = SpecSelection_DEEP2_LSST.make_stage()
+    degrader_DEEP2_LSST(data)
+    degrader_DEEP2_LSST.__repr__()
+
+    os.remove(degrader_DEEP2_LSST.get_output(degrader_DEEP2_LSST.get_aliased_tag("output"), final_name=True))
+
     degrader_VVDSf02 = SpecSelection_VVDSf02.make_stage()
     degrader_VVDSf02(data)
     degrader_VVDSf02.__repr__()
@@ -551,5 +557,3 @@ def test_lya_degrader(data, compute_uv_slope, optical_depth_interpolator):
     assert outputs.shape[0] == data.data.shape[0]
     # check output u band mag is >= input u-band mag:
     assert ((outputs["u"] - data.data["u"])>=0).all()
-
-

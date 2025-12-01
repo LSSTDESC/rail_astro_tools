8dce9ef60403f3f6d143c2d1d0a53a4fb8f0f906
Eric Charles <badass@stanford.edu>
2025-12-01 15:59:27 -0800

    Issue/19/degrade pipes (#80)
    * Allow parallel mode for truth_to_observed and apply_phot_errors pipeline, and switch to using catalog_tag
    
    * switch bands from str to list
    
    * apply tag when testing pipeline
    
    * use catalog names for spectroscopic_selection_pipeline.py
    
    * updated rail_base requirement

--- tests/astro_tools/test_pipline.py
+++ tests/astro_tools/test_pipline.py
@@ -1,4 +1,6 @@
 import os
+from rail.utils.testing_utils import build_and_read_pipeline
+from rail.utils import catalog_utils
 
 import pytest
 from rail.utils.testing_utils import build_and_read_pipeline
@@ -7,17 +9,16 @@ from rail.utils.testing_utils import build_and_read_pipeline
 @pytest.mark.parametrize(
     "pipeline_class, options",
     [
-        ("rail.pipelines.degradation.apply_phot_errors.ApplyPhotErrorsPipeline", {}),
-        ("rail.pipelines.degradation.blending.BlendingPipeline", {}),
-        (
-            "rail.pipelines.degradation.spectroscopic_selection_pipeline.SpectroscopicSelectionPipeline",
-            {},
-        ),
-        (
-            "rail.pipelines.degradation.truth_to_observed.TruthToObservedPipeline",
-            {"blending": True},
-        ),
-    ],
+        ('rail.pipelines.degradation.apply_phot_errors.ApplyPhotErrorsPipeline', {}),
+        ('rail.pipelines.degradation.apply_phot_errors.ApplyPhotErrorsPipeline', {'parallel':True}),
+        ('rail.pipelines.degradation.blending.BlendingPipeline', {}),
+        ('rail.pipelines.degradation.spectroscopic_selection_pipeline.SpectroscopicSelectionPipeline', {}),
+        ('rail.pipelines.degradation.truth_to_observed.TruthToObservedPipeline', {}),
+        ('rail.pipelines.degradation.truth_to_observed.TruthToObservedPipeline', {'blending':True}),
+        ('rail.pipelines.degradation.truth_to_observed.TruthToObservedPipeline', {'parallel':True}),
+        ('rail.pipelines.degradation.truth_to_observed.TruthToObservedPipeline', {'blending':True, 'parallel':True}),
+    ]
 )
 def test_build_and_read_pipeline(pipeline_class, options):
+    catalog_utils.apply_defaults('com_cam')
     build_and_read_pipeline(pipeline_class, **options)

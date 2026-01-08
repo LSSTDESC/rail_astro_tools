58b593a460a17f016afb04bf7a4ae24c1d257411
Eric Charles <badass@stanford.edu>
2026-01-07 18:05:10 -0800

    fix: match names in dereddener stages to error models in pipelines

--- src/rail/pipelines/degradation/apply_phot_errors.py
+++ src/rail/pipelines/degradation/apply_phot_errors.py
@@ -12,13 +12,19 @@ from rail.core.stage import RailStage, RailPipeline
 
 import ceci
 
-from rail.core.utils import RAILDIR
+from rail.core.utils import RAILDIR, find_rail_file
+from rail.core.common_params import SHARED_PARAMS
 from rail.utils import catalog_utils
 
 if 'PZ_DUSTMAP_DIR' not in os.environ:
     os.environ['PZ_DUSTMAP_DIR'] = '.'
 
 dustmap_dir = os.path.expandvars("${PZ_DUSTMAP_DIR}")
+
+OBS_MASK_FILE = find_rail_file("examples_data/creation_data/data/survey_conditions/DC2-mask-neg-nside-128.fits")
+OBS_WEIGHT_FILE = find_rail_file("examples_data/creation_data/data/survey_conditions/DC2-dr6-galcounts-i20-i25.3-nside-128.fits")
+OBS_M5_I_BAND = find_rail_file("examples_data/creation_data/data/survey_conditions/minion_1016_dc2_Median_fiveSigmaDepth_i_and_nightlt1825_HEAL.fits")
+
 
 
 ERROR_MODELS = dict(
@@ -33,6 +39,22 @@ ERROR_MODELS = dict(
             hdf5_groupname='',
         )
     ),
+    lsst_obs_cond = dict(
+        ErrorModel='ObsCondition',
+        Module='rail.creation.degraders.observing_conditions_degrader',
+        Bands=['u', 'g', 'r', 'i', 'z', 'y'],
+        Overrides=dict(
+            hdf5_groupname='',
+            mask=OBS_MASK_FILE,
+            weight=OBS_WEIGHT_FILE,
+            map_dict=dict(
+                m5=dict(
+                    i=OBS_M5_I_BAND
+                )
+            )
+        )
+
+    )
     #roman = dict(
     #    ErrorModel='RomanErrorModel',
     #    Module='rail.creation.degraders.photometric_errors',
@@ -70,12 +92,16 @@ class ApplyPhotErrorsPipeline(RailPipeline):
         )
         previous_stage = self.reddener
         full_rename_dict = catalog_utils.get_active_tag().band_name_dict()
+        full_a_env_dict = SHARED_PARAMS.band_a_env.copy()
+        
         for key, val in error_models.items():
             error_model_class = ceci.PipelineStage.get_stage(val['ErrorModel'], val['Module'])
             if 'Bands' in val:
                 rename_dict = {band_: full_rename_dict[band_] for band_ in val['Bands']}
+                a_env_dict = {band_: full_a_env_dict[band_] for band_ in val['Bands']}                
             else:  # pragma: no cover
                 rename_dict = full_rename_dict
+                a_env_dict = full_a_env_dict                
             overrides = val.get('Overrides', {})
             the_error_model = error_model_class.make_and_connect(
                 name=f'error_model_{key}',
@@ -89,6 +115,7 @@ class ApplyPhotErrorsPipeline(RailPipeline):
                     name=f'deredden_{key}',
                     dustmap_dir=dustmap_dir,
                     connections=dict(input=the_error_model.io.output),
+                    band_a_env=a_env_dict,                    
                     copy_all_cols=True,
                 )
                 self.add_stage(the_dereddener)
